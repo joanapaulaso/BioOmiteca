e48af3a3e2d72021600a4d640dffd05096c2426f
joanapaulasoliveira <joanapaulasoliveira@gmail.com>
2021-09-20 17:17:26 -0300

    filesystem update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://operageo.com.br'),
+    'url' => env('APP_URL', 'http://bioomiteca.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
