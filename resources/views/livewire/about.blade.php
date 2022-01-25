ca94a953726d21dddef1b7932a705c1658fea3f5
Joana Paula Oliveira <joanapaulasoliveira@gmail.com>
2022-01-25 09:10:49 -0300

    about update

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -1,30 +1,32 @@
 <div class="mt-32 mb-20 px-4 lg:px-48">
     <div class="rouded-lg shadow-md p-20 text-center bg-gradient-to-r from-cyan-100 via-purple-100 to-blue-50">
         <p>
-            A <strong>BioOmiteca Mata Atlântica</strong> é uma plataforma digital de integração de dados metabolômicos e biogeográficos de espécies
-            da Mata Atlântica. Mas o que isso quer dizer? 🤔
+            <strong>BioOmiteca Mata Atlântica</strong> is a digital platform for the integration of metabolomic and biogeographic data on species from the Atlantic Forest. But what does that mean? 🤔
         </p>
         <p class="mt-12">
-            A integração desses tipos de dados tem uma finalidade muito simples e, ao mesmo tempo, imprescindível:
-            Com a metabolômica, difundimos o conhecimento sobre o potencial bioeconômico que as espécies da Mata
-            Atlântica têm. Com as modelagens biogeográficas, analisamos as áreas de adequabilidade das espécies
-            dentro de um recorte geográfico específico. A integração desses dados, oferece, portanto, a oportunidade
-            de conhecermos as espécies presentes na riquíssima biodiversidade da Mata Atlântica através da caracterização
-            das moléculas com potencial biotecnológico que produzem, bem como de sabermos se a atual rede de Unidades de
-            Conservação é efetiva na proteção dessas espécies. Por outro lado, áreas de adequabilidade das espécies
-            representam locais com potencial para se realizar o cultivo sustentável.
+            The integration of these types of data has a very simple and, at the same time, essential purpose:
+            With metabolomics, we spread knowledge about the bioeconomic potential that species from the Mata
+            Atlantic have. With the biogeographic modeling, we analyzed the areas of suitability of the species
+            within a specific geographic area. The integration of these data, therefore, offers the opportunity
+            to know the species present in the rich biodiversity of the Atlantic Forest through the characterization
+            of the molecules with biotechnological potential that they produce, as well as whether the current network of Units of
+            Conservation is effective in protecting these species. On the other hand, areas of suitability of species
+            represent places with the potential to carry out sustainable cultivation.
+        </p>
+        <p>
+            🧐 We are still developing the ecological modeling page, though! ✍
         </p>
         <p class="mt-12 font-mono text-lg font-semibold">
-            Nossa premissa é de introduzir um novo paradigma no setor de produtos naturais: a difusão de conhecimento
-            sobre o potencial bioeconômico de espécies da Mata Atlântica deve ser feito juntamente à divulgação de
-            informações imprescindíveis à sua conservação e uso sustentável.
+            Our premise is to introduce a new paradigm in the natural products sector: the diffusion of knowledge
+            on the bioeconomic potential of Atlantic Forest species should be done together with the dissemination of
+            essential information for its conservation and sustainable use.
         </p>
         <p class="mt-12">
-            O público-alvo é o mais amplo possível: público geral, agricultores, membros de comunidades tradicionais,
-            possoas do poder público ligadas ao setor ambiental, dentre outros.
+            The target audience is the widest possible: general public, farmers, members of traditional communities,
+            public authorities linked to the environmental sector, among others.
         </p>
         <p class="mt-12">
-            👋 Seja muito bem-vindo(a)!
+            👋 You are very welcome here!
         </p>
     </div>
 </div>
